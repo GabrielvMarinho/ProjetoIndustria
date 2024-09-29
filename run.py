79e1e68b927f68c9784a7affde92dcc32e2bd936
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2024-09-29 15:21:06 -0300

    asd
    asd

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from app import create_app
-import os
 # chama a função do app que cria tudo
 flask_app = create_app()
 # inicia o app
 if __name__ == "__main__":
-    flask_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    flask_app.run()
