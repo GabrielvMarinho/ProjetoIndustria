82a563ac78ffb4053fa214cde9e82f1d87b6d8ec
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2024-09-29 21:11:06 -0300

    threading

--- run.py
+++ run.py
@@ -1,6 +1,23 @@
 from app import create_app
+from time import sleep
+import threading
 # chama a função do app que cria tudo
 app = create_app()
+
+def tarefa():
+    cont = 0
+    while True:
+        print("it should be good")
+        print(cont)
+        cont =cont+1
+        sleep(5)
+def startThread():
+    thread = threading.Thread(target=tarefa)
+    thread.daemon = True
+    thread.start()
+
 # inicia o app 
+
 if __name__ == "__main__":
+    startThread()
     app.run()
