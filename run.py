1290e6e075520e6931831b009ab0aca231808a55
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2024-09-29 21:30:43 -0300

    logging

--- run.py
+++ run.py
@@ -1,23 +1,28 @@
 from app import create_app
 from time import sleep
 import threading
+import logging
+
+# Configurar o logging para exibir no console (STDOUT)
+logging.basicConfig(level=logging.INFO)
+
 # chama a função do app que cria tudo
 app = create_app()
 
 def tarefa():
     cont = 0
     while True:
-        print("it should be good")
-        print(cont)
-        cont =cont+1
+        logging.info("it should be good")
+        logging.info(f"Cont: {cont}")  # Usando logging ao invés de print
+        cont += 1
         sleep(5)
+
 def startThread():
     thread = threading.Thread(target=tarefa)
     thread.daemon = True
     thread.start()
 
-# inicia o app 
-
+# inicia o app
 if __name__ == "__main__":
     startThread()
     app.run()
